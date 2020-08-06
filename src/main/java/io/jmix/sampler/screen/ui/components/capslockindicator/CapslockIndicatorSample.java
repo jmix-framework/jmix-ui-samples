7c6a37fc509e2f717d443c151efac0e91aa227cd
Gleb Durygin <g.durygin@haulmont.com>
2020-08-06 18:14:43 +0400

    Add sample for CapslockIndicator component #1

---
+++ src/main/java/io/jmix/sampler/screen/ui/components/capslockindicator/CapslockIndicatorSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.components.capslockindicator;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("capslock-indicator")
+@UiDescriptor("capslock-indicator.xml")
+public class CapslockIndicatorSample extends ScreenFragment {
+}
