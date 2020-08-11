791150f6c60230e01d011924ae6402368cdd7d5a
Gleb Durygin <g.durygin@haulmont.com>
2020-08-11 17:16:33 +0400

    Add samples for Link component #1

---
+++ src/main/java/io/jmix/sampler/screen/ui/components/link/LinkSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.components.link;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("link-simple")
+@UiDescriptor("link-simple.xml")
+public class LinkSimpleSample extends ScreenFragment {
+}
