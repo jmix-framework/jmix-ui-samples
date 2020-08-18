a7c6807d7b6e93e42bf94a5cf5905790e1c862dd
Gleb Durygin <g.durygin@haulmont.com>
2020-08-18 11:38:58 +0400

    Add samples for Fragment component #1

---
+++ src/main/java/io/jmix/sampler/screen/ui/containers/fragment/FragmentSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.containers.fragment;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("fragment-simple")
+@UiDescriptor("fragment-simple.xml")
+public class FragmentSimpleSample extends ScreenFragment {
+}
