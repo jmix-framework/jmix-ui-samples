0388e1608ec1820ea6a227d6b4972881fa7753a9
Gleb Durygin <g.durygin@haulmont.com>
2020-08-18 13:30:46 +0400

    Add samples for SplitPanel component #1

---
+++ src/main/java/io/jmix/sampler/screen/ui/containers/splitpanel/dockable/SplitPanelDockableSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.containers.splitpanel.dockable;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("splitpanel-dockable")
+@UiDescriptor("splitpanel-dockable.xml")
+public class SplitPanelDockableSample extends ScreenFragment {
+}
---
+++ src/main/java/io/jmix/sampler/screen/ui/containers/splitpanel/simple/SplitPanelSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.containers.splitpanel.simple;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("splitpanel-simple")
+@UiDescriptor("splitpanel-simple.xml")
+public class SplitPanelSimpleSample extends ScreenFragment {
+}
