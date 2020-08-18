2ce169890af9d9b08c55e7dc69fb66aa98fa8b23
Gleb Durygin <g.durygin@haulmont.com>
2020-08-18 11:19:46 +0400

    Add samples for CssLayout component #1

---
+++ src/main/java/io/jmix/sampler/screen/ui/containers/csslayout/CssLayoutResponsiveSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.containers.csslayout;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("csslayout-responsive")
+@UiDescriptor("csslayout-responsive.xml")
+public class CssLayoutResponsiveSample extends ScreenFragment {
+}
