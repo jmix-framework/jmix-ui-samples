75e227aac2749c3363d1acd2e99f89351aa9338a
Gleb Durygin <g.durygin@haulmont.com>
2020-08-18 12:42:07 +0400

    Add samples for GroupBoxLayout component #1

---
+++ src/main/java/io/jmix/sampler/screen/ui/containers/groupboxlayout/collapsable/GroupBoxLayoutCollapsableSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.containers.groupboxlayout.collapsable;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("groupboxlayout-collapsable")
+@UiDescriptor("groupboxlayout-collapsable.xml")
+public class GroupBoxLayoutCollapsableSample extends ScreenFragment {
+}
---
+++ src/main/java/io/jmix/sampler/screen/ui/containers/groupboxlayout/simple/GroupBoxLayoutSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.containers.groupboxlayout.simple;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("groupboxlayout-simple")
+@UiDescriptor("groupboxlayout-simple.xml")
+public class GroupBoxLayoutSimpleSample extends ScreenFragment {
+}
