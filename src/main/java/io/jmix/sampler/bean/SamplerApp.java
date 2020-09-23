c74c53d25167b432801a7df39e69c9b590be66e1
Vlad Minaev <minaev@haulmont.com>
2020-09-23 17:22:23 +0400

    Refactor AppUI (UIScope) haulmont/jmix-ui#57

--- src/main/java/io/jmix/sampler/bean/SamplerApp.java
+++ src/main/java/io/jmix/sampler/bean/SamplerApp.java
@@ -1,7 +1,9 @@
 package io.jmix.sampler.bean;
 
+import com.vaadin.spring.annotation.VaadinSessionScope;
 import io.jmix.ui.JmixApp;
 
+@VaadinSessionScope
 public class SamplerApp extends JmixApp {
     public void applyUserTheme(String appWindowTheme) {
         applyTheme(appWindowTheme);
