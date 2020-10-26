f1986fd757a908c33bf75b816b2f24908728fd82
Gleb Durygin <durygin@haulmont.com>
2020-10-26 14:52:37 +0400

    No formatter applied for DataGridFormatterSample Haulmont/jmix-sampler#17

--- src/main/java/io/jmix/sampler/screen/ui/components/table/formatter/LowercaseFormatter.java
+++ src/main/java/io/jmix/sampler/screen/ui/components/table/formatter/LowercaseFormatter.java
@@ -1,21 +1,26 @@
-package io.jmix.sampler.screen.ui.components.datagrid.formatter;
+package io.jmix.sampler.screen.ui.components.table.formatter;
 
+import io.jmix.core.Messages;
 import io.jmix.ui.component.formatter.Formatter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
 
-@Component("sampler_UppercaseFormatter")
+@Component("sampler_LowercaseFormatter")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class UppercaseFormatter implements Formatter<String> {
+public class LowercaseFormatter implements Formatter<String> {
+
+    @Autowired
+    protected Messages messages;
 
     @Nullable
     @Override
     public String apply(@Nullable String value) {
         return value != null
-                ? value.toUpperCase()
+                ? value.toLowerCase()
                 : null;
     }
 }
--- src/main/java/io/jmix/sampler/screen/ui/components/table/formatter/UppercaseFormatter.java
+++ src/main/java/io/jmix/sampler/screen/ui/components/table/formatter/UppercaseFormatter.java
@@ -1,26 +1,21 @@
-package io.jmix.sampler.screen.ui.components.datagrid.formatter;
+package io.jmix.sampler.screen.ui.components.table.formatter;
 
-import io.jmix.core.Messages;
 import io.jmix.ui.component.formatter.Formatter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
 
-@Component("sampler_LowercaseFormatter")
+@Component("sampler_UppercaseFormatter")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class LowercaseFormatter implements Formatter<String> {
-
-    @Autowired
-    protected Messages messages;
+public class UppercaseFormatter implements Formatter<String> {
 
     @Nullable
     @Override
     public String apply(@Nullable String value) {
         return value != null
-                ? value.toLowerCase()
+                ? value.toUpperCase()
                 : null;
     }
 }
