600ef92e38bd5c91d27142753e7e43178aadba31
Gleb Gorelov <gorelov@haulmont.com>
2020-12-04 16:46:01 +0400

    Update Drawer sample

--- src/main/java/io/jmix/sampler/screen/ui/containers/drawer/DrawerSample.java
+++ src/main/java/io/jmix/sampler/screen/ui/containers/drawer/DrawerSample.java
@@ -1,8 +1,6 @@
 package io.jmix.sampler.screen.ui.containers.drawer;
 
 import io.jmix.ui.component.Button;
-import io.jmix.ui.component.CssLayout;
-import io.jmix.ui.component.Label;
 import io.jmix.ui.component.mainwindow.Drawer;
 import io.jmix.ui.screen.ScreenFragment;
 import io.jmix.ui.screen.Subscribe;
@@ -12,19 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("drawer-sample")
 @UiDescriptor("drawer-sample.xml")
-public class DrawerSample extends ScreenFragment  {
+public class DrawerSample extends ScreenFragment {
 
     @Autowired
     private Drawer drawer;
-    @Autowired
-    private Label<String> contentLabel;
-    @Autowired
-    private Label<String> headerLabel;
 
     @Subscribe("toggleDrawerButton")
     private void onToggleDrawer(Button.ClickEvent event) {
         drawer.toggle();
-        headerLabel.setVisible(!drawer.isCollapsed());
-        contentLabel.setVisible(!drawer.isCollapsed());
     }
 }
