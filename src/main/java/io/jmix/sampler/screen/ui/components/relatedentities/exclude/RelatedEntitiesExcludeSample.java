b9deee57af86d4ef22767105876d9c696b74c54d
Gleb Durygin <durygin@haulmont.com>
2021-02-10 16:22:50 +0400

    RelatedEntities must take into account the Filter component Haulmont/jmix-ui#52

---
+++ src/main/java/io/jmix/sampler/screen/ui/components/relatedentities/exclude/RelatedEntitiesExcludeSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.components.relatedentities.exclude;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("relatedentities-exclude")
+@UiDescriptor("relatedentities-exclude.xml")
+public class RelatedEntitiesExcludeSample extends ScreenFragment {
+}
---
+++ src/main/java/io/jmix/sampler/screen/ui/components/relatedentities/simple/RelatedEntitiesSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.components.relatedentities.simple;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("relatedentities-simple")
+@UiDescriptor("relatedentities-simple.xml")
+public class RelatedEntitiesSimpleSample extends ScreenFragment {
+}
