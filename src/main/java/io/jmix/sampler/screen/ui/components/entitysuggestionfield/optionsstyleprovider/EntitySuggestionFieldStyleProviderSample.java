06a1c04bfd07a3f02e3ff0d161413279131a915c
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-02-20 16:27:23 +0400

    Simplify DataManager usage

--- src/main/java/io/jmix/sampler/screen/ui/components/entitysuggestionfield/optionsstyleprovider/EntitySuggestionFieldStyleProviderSample.java
+++ src/main/java/io/jmix/sampler/screen/ui/components/entitysuggestionfield/optionsstyleprovider/EntitySuggestionFieldStyleProviderSample.java
@@ -3,16 +3,10 @@ package io.jmix.sampler.screen.ui.components.entitysuggestionfield.optionsstylep
 import io.jmix.core.DataManager;
 import io.jmix.core.Metadata;
 import io.jmix.core.QueryUtils;
-import io.jmix.core.common.util.ParamsMap;
-import io.jmix.core.querycondition.JpqlCondition;
 import io.jmix.sampler.entity.Customer;
 import io.jmix.sampler.entity.Order;
 import io.jmix.ui.model.InstanceContainer;
-import io.jmix.ui.screen.Install;
-import io.jmix.ui.screen.ScreenFragment;
-import io.jmix.ui.screen.Subscribe;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -42,8 +36,7 @@ public class EntitySuggestionFieldStyleProviderSample extends ScreenFragment {
     protected List<Customer> entitySuggestionFieldSearchExecutor(String searchString, Map<String, Object> searchParams) {
         searchString = QueryUtils.escapeForLike(searchString);
         return dataManager.load(Customer.class)
-                .condition(JpqlCondition.createWithParameters("e.name like :name order by e.name escape '\\'",
-                        null, ParamsMap.of("name", "%" + searchString + "%")))
+                .query("e.name like ?1 escape '\\' order by e.name", "(?i)%" + searchString + "%")
                 .list();
     }
 
