7f05b02da021052ab303afe1f8279abb7e64a754
Roman Pinyazhin <pinyazhin@haulmont.com>
2021-06-15 12:26:32 +0400

    Add PropertyFilter component sample #12

---
+++ src/main/java/io/jmix/sampler/screen/ui/components/propertyfilter/simple/PropertyFilterSimpleSample.java
@@ -0,0 +1,10 @@
+package io.jmix.sampler.screen.ui.components.propertyfilter.simple;
+
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("propertyfilter-simple")
+@UiDescriptor("propertyfilter-simple.xml")
+public class PropertyFilterSimpleSample extends ScreenFragment {
+}
