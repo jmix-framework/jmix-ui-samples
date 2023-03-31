f05d01be924b305c94d12b599d9bba06678d0248
Roman Pinyazhin <pinyazhin@haulmont.com>
2023-03-31 17:21:39 +0400

    DataGridDetailsGeneratorSample throws an exception while trying to open details

--- src/main/java/io/jmix/sampler/screen/ui/components/datagrid/detailsgenerator/DataGridDetailsGeneratorService.java
+++ src/main/java/io/jmix/sampler/screen/ui/components/datagrid/detailsgenerator/DataGridDetailsGeneratorService.java
@@ -1,15 +1,8 @@
 package io.jmix.sampler.screen.ui.components.datagrid.detailsgenerator;
 
-import io.jmix.core.DataManager;
-import io.jmix.core.FetchPlan;
-import io.jmix.core.FetchPlanBuilder;
-import io.jmix.core.FetchPlanRepository;
-import io.jmix.core.LoadContext;
-import io.jmix.core.Metadata;
+import io.jmix.core.*;
 import io.jmix.core.metamodel.model.MetaClass;
 import io.jmix.sampler.entity.OrderItem;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -18,20 +11,26 @@ import java.util.UUID;
 @Component("sampler_DataGridDetailsGeneratorService")
 public class DataGridDetailsGeneratorService {
 
-    @Autowired
-    protected DataManager dataManager;
-    @Autowired
-    protected Metadata metadata;
-    @Autowired
-    protected FetchPlanRepository fetchPlanRepository;
-    @Autowired
-    protected ObjectProvider<FetchPlanBuilder> fetchPlanBuilder;
+    protected final DataManager dataManager;
+    protected final Metadata metadata;
+    protected final FetchPlanRepository fetchPlanRepository;
+    protected final FetchPlans fetchPlans;
+
+    public DataGridDetailsGeneratorService(DataManager dataManager,
+                                           Metadata metadata,
+                                           FetchPlanRepository fetchPlanRepository,
+                                           FetchPlans fetchPlans) {
+        this.dataManager = dataManager;
+        this.metadata = metadata;
+        this.fetchPlanRepository = fetchPlanRepository;
+        this.fetchPlans = fetchPlans;
+    }
 
     public List<OrderItem> loadOrderItemsById(UUID orderId) {
         MetaClass metaClass = metadata.getClass(OrderItem.class);
         LoadContext.Query query = new LoadContext.Query("select e from sampler_OrderItem e where e.order.id = :orderId")
                 .setParameter("orderId", orderId);
-        FetchPlan fetchPlan = fetchPlanBuilder.getObject(OrderItem.class)
+        FetchPlan fetchPlan = fetchPlans.builder(OrderItem.class)
                 .addFetchPlan(fetchPlanRepository.getFetchPlan(OrderItem.class, "_local"))
                 .add("product", "_local")
                 .add("order", "_local")
