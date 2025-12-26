e99654b9d4c7bdaddbceb91f5cfdd46288cd4f5d
Dmitry Kremnev <kremnev.dima@mail.ru>
2025-12-26 14:55:42 +0400

    Refactor deprecated code

--- src/main/java/io/jmix/uisamples/view/flowui/containers/card/themevariant/CardThemeVariantSample.java
+++ src/main/java/io/jmix/uisamples/view/flowui/containers/card/themevariant/CardThemeVariantSample.java
@@ -42,10 +42,11 @@ public class CardThemeVariantSample extends StandardView {
     @ViewComponent
     private JmixCheckboxGroup<String> themeGroup;
 
+    @ViewComponent
+    private MessageBundle messageBundle;
+
     @Autowired
     private UiComponents uiComponents;
-    @Autowired
-    private MessageBundle messageBundle;
     @Autowired
     private Messages messages;
 
